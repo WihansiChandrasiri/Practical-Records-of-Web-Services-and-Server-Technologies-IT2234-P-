2a431bbecae4e6589267a159cfb2484d3a9849a5
WihansiChandrasiri <wihansichandrasiri@gmail.com>
2025-03-13 00:07:50 +0530

    Create PrintNumbers.js
    Here we use a for loops to print sequences of numbers.

---
+++ PrintNumbers.js
@@ -0,0 +1,29 @@
+//Printing numbers from 1 to 10
+for(let i=1; i<10; i++)
+{
+	console.log(i + " ")
+}
+
+console.log("\n")
+
+//Printing the odd numbers from 1 to 10
+for(let i=1; i<10; i++)
+{
+	if(i%2 == 1)
+	{
+		console.log(i + " ")
+	}
+}
+
+console.log("\n")
+
+//Printing even numbers from 1 to 10
+for(let i=1; i<10; i++)
+{
+	if(i%2 == 0)
+	{
+		console.log(i + " ")
+	}
+}
+console.log("\n")
+
