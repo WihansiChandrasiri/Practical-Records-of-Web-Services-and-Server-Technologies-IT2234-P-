ef8a444930d96588b11087db30117f14ea640adc
WihansiChandrasiri <wihansichandrasiri@gmail.com>
2025-03-13 00:56:55 +0530

    Create PrintLeftAndRight.js

---
+++ PrintLeftAndRight.js
@@ -0,0 +1,18 @@
+let num = 15
+
+mid = parseInt(num/2)+1
+
+for(let i = mid; i>0; i--)
+{
+	console.log(i);
+}
+console.log("-")
+
+console.log (mid);
+
+console.log("-")
+for(let x=num; x>mid; x--)
+{
+	console.log(x);
+}
+console.log("\n")
