c6bab71dd7655218ea99d3b77d7ff1e8b9602d7e
WihansiChandrasiri <wihansichandrasiri@gmail.com>
2025-03-31 20:28:03 +0530

    Add files via upload

---
+++ studentdb.js
@@ -0,0 +1,9 @@
+let students = [
+    { Reg_No: "2021ict01", name: "Sham", course: "IT", gender: "Female" },
+    { Reg_No: "2021ict66", name: "Ram", course: "Bio", gender: "Male" },
+    { Reg_No: "2021ict12", name: "Rajeev", course: "Math", gender: "Male" },
+    { Reg_No: "2021ict99", name: "Meena", course: "IT", gender: "Female" },
+    { Reg_No: "2021ict100", name: "Kumar", course: "Math", gender: "Male" }
+];
+
+module.exports=students;
