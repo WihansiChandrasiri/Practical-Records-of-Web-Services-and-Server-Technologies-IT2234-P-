0c3826f6350f17300152038afc74c7d224a58e4b
WihansiChandrasiri <wihansichandrasiri@gmail.com>
2025-04-11 00:06:17 +0530

    Create studentdb.js

---
+++ student/studentdb.js
@@ -0,0 +1,10 @@
+let students = 
+[
+			  {regno:'2021ICT01', name:'James',gender:'Male',age:21, course:'IT'},
+			  {regno:'2021ICT02', name:'Ravi',gender:'Male',age:22, course:'AMC'},
+			  {regno:'2021ICT03', name:'Gayatri',gender:'Female',age:21, course:'IT'},
+        {regno:'2021ICT04', name:'Vimala',gender:'Female',age:21, course:'AMC'},
+        {regno:'2021ICT05', name:'Arjun',gender:'Male',age:21, course:'IT'}
+];
+
+module.exports=students
